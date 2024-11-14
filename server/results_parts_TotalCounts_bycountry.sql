f94b721a4632551e7ae34f83ecf9c607347695a0
Sergei Dorogin <segy@google.com>
2024-11-14 15:02:07 +0400

    [sql] optimization for TotalCounts subquery with filter by country
    Change-Id: I152d33a058e8b4640ab843d6277e6ec858fa4bab

--- server/results_parts_TotalCounts_bycountry.sql
+++ server/results_parts_TotalCounts_bycountry.sql
@@ -1,3 +1,49 @@
+    WITH DailyUserCounts AS (
+      SELECT
+        _TABLE_SUFFIX as table_date,
+        'test' as group_type,
+        COUNT(DISTINCT user) as user_count
+      FROM `{test_users_table}`
+      INNER JOIN `{all_users_table}`
+        USING (user)
+      WHERE
+        _TABLE_SUFFIX IN (
+          SELECT FORMAT_DATE('%E4Y%m%d', DATE(`date`)) AS day
+          FROM `{audiences_log}`
+          WHERE name = '{audience_name}'
+        )
+        AND app_id = '{app_id}'
+        {SEARCH_CONDITIONS}
+      GROUP BY _TABLE_SUFFIX
+
+      UNION ALL
+
+      SELECT
+        _TABLE_SUFFIX as table_date,
+        'control' as group_type,
+        COUNT(DISTINCT user) as user_count
+      FROM `{control_users_table}`
+      INNER JOIN `{all_users_table}`
+        USING (user)
+      WHERE
+        _TABLE_SUFFIX IN (
+          SELECT FORMAT_DATE('%E4Y%m%d', DATE(`date`)) AS day
+          FROM `{audiences_log}`
+          WHERE name = '{audience_name}'
+        )
+        AND app_id = '{app_id}'
+        {SEARCH_CONDITIONS}
+      GROUP BY _TABLE_SUFFIX
+    )
+    SELECT
+      PARSE_DATE("%Y%m%d", table_date) AS day,
+      SUM(CASE WHEN group_type = 'test' THEN user_count END)
+        OVER(ORDER BY table_date ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) as total_test_user_count,
+      SUM(CASE WHEN group_type = 'control' THEN user_count END)
+        OVER(ORDER BY table_date ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) as total_control_user_count
+    FROM DailyUserCounts
+
+
     WITH
       DailyUniqueTestUsers AS (
         SELECT
